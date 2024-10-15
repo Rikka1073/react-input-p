d3601947eb3d3cf0851170c4a976270272487a8c
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-15 19:53:45 +0900

    input追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 function App() {
+  const [checked, setChecked] = useState(false);
+  const onChangeClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = event.target.checked;
+    setChecked(checked);
+  };
+
   return (
     <>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div className="text-center mt-5">
+        <input type="checkbox" id="name" name="name" checked={checked} onChange={onChangeClick} />
+        <label htmlFor="name">名前</label>
+        <div className="font-bold">{checked ? <p>チェックされています</p> : null}</div>
+      </div>
     </>
   );
 }
